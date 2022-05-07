a3e1f151f1f6af7387522f4375c7898fd5fc30d6
Tristan Boyd <trisboyd@gmail.com>
2022-05-07 13:28:20 -0600

    'footer includes links'

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,38 @@
-import { FooterSection, FooterText } from "./styledFooter";
+import { FooterSection, FooterText, Image, Link, Links } from "./styledFooter";
+import twitter from '../../images/twitter.png';
+import youtube from '../../images/youtube.jpg';
+import github from '../../images/github.jpg';
+import linkedin from '../../images/linkedin.png';
+import { links } from '../../constants/footer/footer';
 
 const Footer = () => {
 
     return (
         <FooterSection>
+            <Links>
+                <Link
+                    href={links.github}
+                    target='_blank'>
+                    <Image src={github} />
+                </Link>
+                <Link
+                    href={links.linkedin}
+                    target='_blank'>
+                    <Image
+                        src={linkedin}
+                        padding='5px' />
+                </Link>
+                <Link
+                    href={links.twitter}
+                    target='_blank'>
+                    <Image src={twitter} />
+                </Link>
+                <Link
+                    href={links.youtube}
+                    target='_blank'>
+                    <Image src={youtube} />
+                </Link>
+            </Links>
             <FooterText>
                 © 2022 | Tristan Boyd
             </FooterText>
